950caecedd73cff8bbab733b7cb935f660ff67fd
yql776476966 <776476966@qq.com>
2016-05-15 12:51:42 +0800

    Start build mvp architecture

---
+++ Todo-mvp/app/src/main/java/com/entrobus/todomvp/BasePresenter.java
@@ -0,0 +1,10 @@
+package com.entrobus.todomvp;
+
+/**
+ * Author：wave
+ * Date：2016/5/15
+ * Description：
+ */
+public interface BasePresenter {
+    void start();
+}
---
+++ Todo-mvp/app/src/main/java/com/entrobus/todomvp/tasks/TasksActivity.java
@@ -0,0 +1,11 @@
+package com.entrobus.todomvp.tasks;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Author：wave
+ * Date：2016/5/15
+ * Description：
+ */
+public class TasksActivity extends AppCompatActivity {
+}
